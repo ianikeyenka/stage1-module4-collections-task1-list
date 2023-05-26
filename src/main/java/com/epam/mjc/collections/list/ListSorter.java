acaaa92a28ae13ff676798290a37ec5b1bfe56b2
Ilya Anikeyenka <20prorok02@gmail.com>
2023-05-26 23:45:02 +0300

    Solution

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        if (Math.pow(Integer.parseInt(a), 2) * 5 + Integer.parseInt(a) * 3 >
+                Math.pow(Integer.parseInt(b), 2) * 5 + Integer.parseInt(b) * 3) {
+            return 1;
+        }
+        return -1;
     }
 }
